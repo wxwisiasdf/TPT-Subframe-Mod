b092d21a6bbecdf74742dc421c402a6c7b7450f1
Simon Robertshaw <simon@hardwired.org.uk>
2013-04-07 12:45:55 +0100

    HTTP req wip

--- src/network/HttpRequest.cpp
+++ src/network/HttpRequest.cpp
@@ -1,1 +1,46 @@
-HttpRequest.cpp
+#include "TcpSocket.h"
+#include "Format.h"
+
+using namespace network;
+
+HttpRequest(std::string url, Method method)
+{
+	this->method = method;
+	socket = new TcpSocket();
+}
+
+HttpRequest & AddHeader(std::string name, std::string value)
+{
+	return *this;
+}
+
+HttpRequest & AddBody(std::string name, std::string value)
+{
+	return AddBody(name, value.data(), value.size());
+}
+
+HttpRequest & AddBody(std::string name, char * data, int length)
+{
+	if(method == POST) {
+
+	}
+	return *this;
+}
+
+void Start()
+{
+	if(method == POST)
+	{
+		AddHeader("Content-Length", format::NumberToString(body.size()));
+	}
+}
+
+void End()
+{
+
+}
+
+bool IsComplete()
+{
+
+}
--- src/network/HttpRequest.h
+++ src/network/HttpRequest.h
@@ -1,12 +1,20 @@
 namespace network
 {
+	class TcpSocket;
 	class HttpRequest
 	{
-		HttpRequest(std::string);
+		Method method;
+		TcpSocket * socket;
+		std::vector<char> body;
+	public:
+		enum Method { GET, POST, HEAD }
+		HttpRequest(std::string url, Method method = GET);
 		void AddHeader(std::string name, std::string value);
+		void AddBody(std::string name, std::string value);
+		void AddBody(std::string name, char * data, int length);
 
 		void Start();
 		void End();
-		bool GetComplete();
+		bool IsComplete();
 	};
 }
